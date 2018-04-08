d0e4205a3f8d2c7f9eb8ad6b6363875bd2e67e55
alexander cabezas <alexander.cabezas.m@gmail.com>
2018-04-08 17:37:10 +0200

    first commit

---
+++ app/src/main/java/com/acabezas/ehealth_patient_app/ControlActivity.java
@@ -0,0 +1,12 @@
+package com.acabezas.ehealth_patient_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
